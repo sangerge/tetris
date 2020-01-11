13b4c61dd893501b00febd60b1568820f0c485ec
桑二哥 <51050197+sangerge@users.noreply.github.com>
2020-01-11 12:42:55 +0800

    Add files via upload

---
+++ res/resources.h
@@ -0,0 +1,34 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 colortheme_bgm[18944];
+extern const u8 logo_bgm[116480];
+extern const u8 title_bgm[70656];
+extern const u8 game_bgm1[84736];
+extern const u8 game_bgm2[73472];
+extern const u8 game_bgm3[156928];
+extern const u8 game_bgm4[88576];
+extern const u8 game_bgm5[76032];
+extern const u8 game_bgm6[45312];
+extern const u8 game_bgm7[74496];
+extern const u8 game_bgm8[56064];
+extern const u8 game_bgm9[81920];
+extern const u8 game_bgm10[84992];
+extern const u8 game_bgm11[59904];
+extern const u8 game_bgm12[75520];
+extern const u8 game_bgm13[90624];
+extern const u8 game_bgm14[94720];
+extern const u8 game_bgm15[72192];
+extern const u8 game_bgm16[175360];
+extern const u8 gameover_sound[30720];
+extern const u8 gameover_bgm[3840];
+extern const u8 showscore_bgm[54784];
+extern const u8 intro_bgm[47360];
+extern const u8 duang[7168];
+extern const u8 ding[1024];
+extern const u8 clear[11264];
+extern const u8 level_up[16384];
+extern const Image info;
+extern const Image intro;
+
+#endif // _RES_RESOURCES_H_
---
+++ res/resources.res
@@ -0,0 +1,33 @@
+XGM colortheme_bgm "colortheme_bgm.vgm" -1
+XGM logo_bgm "logo.vgm" -1
+XGM title_bgm "title.vgm" -1
+
+XGM game_bgm1 "game_bgm1.vgm" -1
+XGM game_bgm2 "game_bgm2.vgm" -1
+XGM game_bgm3 "game_bgm3.vgm" -1
+XGM game_bgm4 "game_bgm4.vgm" -1
+XGM game_bgm5 "game_bgm5.vgm" -1
+XGM game_bgm6 "game_bgm6.vgm" -1
+XGM game_bgm7 "game_bgm7.vgm" -1
+XGM game_bgm8 "game_bgm8.vgm" -1
+XGM game_bgm9 "game_bgm9.vgm" -1
+XGM game_bgm10 "game_bgm10.vgm" -1
+XGM game_bgm11 "game_bgm11.vgm" -1
+XGM game_bgm12 "game_bgm12.vgm" -1
+XGM game_bgm13 "game_bgm13.vgm" -1
+XGM game_bgm14 "game_bgm14.vgm" -1
+XGM game_bgm15 "game_bgm15.vgm" -1
+XGM game_bgm16 "game_bgm16.vgm" -1
+
+XGM gameover_sound "gameover_sound.vgm" -1
+XGM gameover_bgm "gameover_bgm.vgm" -1
+XGM showscore_bgm "showscore.vgm" -1
+XGM intro_bgm "intro.vgm" -1
+
+IMAGE info "info.png" 0
+IMAGE intro "intro.png" 0
+
+WAV duang "duang.wav" 5
+WAV ding "ding.wav" 5
+WAV clear "clear.wav" 5
+WAV level_up "level_up.wav" 5
